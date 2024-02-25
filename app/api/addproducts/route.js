6a3bf047c4ce981482a977da54545a20dbd29405
Tanishq Solanki <officialxinov@gmail.com>
2024-02-25 19:24:08 +0530

    initial commit

--- app/api/addproducts/route.js
+++ app/api/addproducts/route.js
@@ -5,11 +5,29 @@ import { NextResponse } from 'next/server';
 // Establish MongoDB connection
 connectDb();
 
-export async function GET(req) {
-    try {
-        const products = await Product.find();
-        return NextResponse.json({ products });
-    } catch (error) {
-        return NextResponse.json({ error: error.message });
+export async function POST(req) {
+    const res = await req.json();
+
+    if (res.length !== 0 && res.length !== undefined && res.length !== null) {
+        for (let i = 0; i < res.length; i++) {
+            let p = new Product({
+                title: res[i].title,
+                slug: res[i].slug,
+                // desc: res[i].desc,
+                dimg: res[i].dimg,
+                img1: res[i].img1,
+                catagory: res[i].catagory,
+                subCatagory: res[i].subCatagory,
+                availableQty: res[i].availableQty,
+                price: res[i].price,
+                sku: res[i].sku,
+            });
+            await p.save();
+        }
+
+        console.log(res)
+        return NextResponse.json({ success: "success" });
+    } else {
+        return NextResponse.json({ error: "Products array is empty" });
     }
 }
